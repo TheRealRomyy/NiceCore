6bcc957340184db5f39b32baee7b776059e8359b
Whire-coder <romeo.pst44@gmail.com>
2021-07-21 17:03:03 +0200

    Add emerald pickaxe and sword

--- src/fr/rome/nicecore/items/OtherStuff.java
+++ src/fr/rome/nicecore/items/OtherStuff.java
@@ -22,6 +22,8 @@ public class OtherStuff {
         this.gapple();
         this.unbreakableSword();
         this.emeraldBowCraft();
+        this.emeraldSword();
+        this.emeraldPickaxe();
     };
 
     public void gapple() {
@@ -68,16 +70,71 @@ public class OtherStuff {
 
     public ItemStack buildEmeraldBow() {
 
-        ItemStack detector = new ItemStack(Material.BOW, 1);
-        ItemMeta detectorM = detector.getItemMeta();
-        detectorM.setDisplayName("§aEmerald Bow");
-        detectorM.setLore(Arrays.asList("§bUn arc parfait", "§6Made by: Rome"));
-        detectorM.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
-        detectorM.addEnchant(Enchantment.ARROW_KNOCKBACK, 2, true);
-        detectorM.addEnchant(Enchantment.ARROW_DAMAGE, 7, true);
-        detectorM.addEnchant(Enchantment.ARROW_FIRE, 3, true);
-        detector.setItemMeta(detectorM);
+        ItemStack emeraldBow = new ItemStack(Material.BOW, 1);
+        ItemMeta emeraldBowM = emeraldBow.getItemMeta();
+        emeraldBowM.setDisplayName("§aEmerald Bow");
+        emeraldBowM.setLore(Arrays.asList("§bUn arc parfait", "§6Made by: Rome"));
+        emeraldBowM.addEnchant(Enchantment.ARROW_INFINITE, 1, true);
+        emeraldBowM.addEnchant(Enchantment.ARROW_KNOCKBACK, 2, true);
+        emeraldBowM.addEnchant(Enchantment.ARROW_DAMAGE, 7, true);
+        emeraldBowM.addEnchant(Enchantment.ARROW_FIRE, 3, true);
+        emeraldBowM.addEnchant(Enchantment.DURABILITY, 3, true);
+        emeraldBowM.addEnchant(Enchantment.MENDING, 1, true);
+        emeraldBow.setItemMeta(emeraldBowM);
 
-        return detector;
+        return emeraldBow;
+    };
+
+    public void emeraldSword() {
+        ItemStack emeraldSword = this.buildEmeraldSword();
+        ShapedRecipe emeraldSwordRecipe = new ShapedRecipe(emeraldSword);
+        emeraldSwordRecipe.shape("EEE", "EBE", "EEE");
+
+        emeraldSwordRecipe.setIngredient('E', Material.EMERALD_BLOCK);
+        emeraldSwordRecipe.setIngredient('B', Material.DIAMOND_SWORD);
+
+        main.getServer().addRecipe(emeraldSwordRecipe);
+    };
+
+    public ItemStack buildEmeraldSword() {
+
+        ItemStack emeraldSword = new ItemStack(Material.DIAMOND_SWORD, 1);
+        ItemMeta emeraldSwordM = emeraldSword.getItemMeta();
+        emeraldSwordM.setDisplayName("§aEmerald Sword");
+        emeraldSwordM.setLore(Arrays.asList("§bUne épée parfaite", "§6Made by: Rome"));
+        emeraldSwordM.addEnchant(Enchantment.FIRE_ASPECT, 2, true);
+        emeraldSwordM.addEnchant(Enchantment.DAMAGE_ALL, 5, true);
+        emeraldSwordM.addEnchant(Enchantment.DURABILITY, 3, true);
+        emeraldSwordM.addEnchant(Enchantment.MENDING, 1, true);
+        emeraldSwordM.addEnchant(Enchantment.KNOCKBACK, 2, true);
+        emeraldSword.setItemMeta(emeraldSwordM);
+
+        return emeraldSword;
+    };
+
+    public void emeraldPickaxe() {
+        ItemStack emeraldPickaxe = this.buildEmeraldPickaxe();
+        ShapedRecipe emeraldPickaxeRecipe = new ShapedRecipe(emeraldPickaxe);
+        emeraldPickaxeRecipe.shape("EEE", "EBE", "EEE");
+
+        emeraldPickaxeRecipe.setIngredient('E', Material.EMERALD_BLOCK);
+        emeraldPickaxeRecipe.setIngredient('B', Material.DIAMOND_PICKAXE);
+
+        main.getServer().addRecipe(emeraldPickaxeRecipe);
+    };
+
+    public ItemStack buildEmeraldPickaxe() {
+
+        ItemStack emeraldPickaxe = new ItemStack(Material.DIAMOND_PICKAXE, 1);
+        ItemMeta emeraldPickaxeM = emeraldPickaxe.getItemMeta();
+        emeraldPickaxeM.setDisplayName("§aEmerald Pickaxe");
+        emeraldPickaxeM.setLore(Arrays.asList("§bUne pioche parfaite", "§6Made by: Rome"));
+        emeraldPickaxeM.addEnchant(Enchantment.DIG_SPEED, 5, true);
+        emeraldPickaxeM.addEnchant(Enchantment.DURABILITY, 3, true);
+        emeraldPickaxeM.addEnchant(Enchantment.MENDING, 1, true);
+        emeraldPickaxeM.addEnchant(Enchantment.LUCK, 3, true);
+        emeraldPickaxe.setItemMeta(emeraldPickaxeM);
+
+        return emeraldPickaxe;
     };
 };
